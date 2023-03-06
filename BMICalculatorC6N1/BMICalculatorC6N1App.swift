d59d32a62cb59aa9ee45fedb17465f196b396b5d
Handy <handy.kwok@gmail.com>
2023-03-06 10:51:13 +0700

    Initial Commit

---
+++ BMICalculatorC6N1/BMICalculatorC6N1App.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorC6N1App.swift
+//  BMICalculatorC6N1
+//
+//  Created by Handy Handy on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorC6N1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
